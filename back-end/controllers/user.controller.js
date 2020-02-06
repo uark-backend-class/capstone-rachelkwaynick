5ce419fdbfcf577cf5a0ceeab86b3e1a7778411f
Rachel Waynick <rachelkwaynick@gmail.com>
2020-02-06 09:09:29 -0600

    some angular messing around, need to split branches better

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -2,27 +2,37 @@ const User = require('../db').User;
 
 // const passport = require('passport');
 
-exports.registerUser = (req, res) => {
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
+exports.registerUser = async (req, res) => {
+    // let firstName = req.body.firstName;
+    // let lastName = req.body.lastName;
     let email = req.body.email;
     let password = req.body.password;
-    let personalBudgetId = req.body.personalBudgetId;
-    let familyBudgetId = req.body.familyBudgetId;
+    // let personalBudgetId = req.body.personalBudgetId;
+    // let familyBudgetId = req.body.familyBudgetId;
 
-    console.log('register function running')
+    User.register(email, password, async (error, registeredUser) => {
 
-    User.register(firstName, lastName, email, password, personalBudgetId, familyBudgetId, (error, registeredUser) => {
+
+
         if (error) {
             console.log(error.message);
             return res.status(500).send(error.message);
         }
+
+
         res.send("Created user: " + email);
+
+        registeredUser.firstName = req.body.firstName;
+        registeredUser.lastName = req.body.lastName;
+        
+        await registeredUser.save();
+
+        
     });
 }
 
 exports.getAllUsers = async (req, res) => {
     let users = await User.findAll();
-    res.json(users);
+    res.json("get all users");
     console.log(users);
 }
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.route('/registerUser')
-    .post(userController.registerUser);
-    // .get(userController.getAllUsers);
+    .post(userController.registerUser)
+    .get(userController.getAllUsers);
 
 // router.route('/createBudget')
 //     .post(budgetController.)
