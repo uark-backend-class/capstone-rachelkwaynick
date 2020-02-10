f92289520c76af3a1ef4da61023ae9339821505f
Rachel Waynick <rachelkwaynick@gmail.com>
2020-02-10 11:06:38 -0600

    registration working with angular

--- front-end/budget-app/src/app/login/login.component.ts
+++ front-end/budget-app/src/app/login/login.component.ts
@@ -31,13 +31,20 @@ export class LoginComponent implements OnInit {
   
 
    onSubmit() {
-    const formData: any = new FormData();
-     formData.append('firstName', this.registerForm.get('firstName').value);
-     formData.append('lastName', this.registerForm.get('lastName').value);
-     formData.append('email', this.registerForm.get('email').value);
-     formData.append('password', this.registerForm.get('password').value);
+    // const formData: any = new FormData();
+    //  formData.append('firstName', this.registerForm.get('firstName').value);
+    //  formData.append('lastName', this.registerForm.get('lastName').value);
+    //  formData.append('email', this.registerForm.get('email').value);
+    //  formData.append('password', this.registerForm.get('password').value);
 
-    this.dataService.postData(this.Url, formData)
+    let firstName = this.registerForm.get('firstName').value;
+    let lastName = this.registerForm.get('lastName').value;
+    let email = this.registerForm.get('email').value;
+    let password = this.registerForm.get('password').value;
+
+
+
+    this.dataService.postData(this.Url, {firstName, lastName, email, password})
       .subscribe(console.log)
   }
 
