44e746a6a5a4dfade072b758d4c96d9c407a5565
Rachel Waynick <rachelkwaynick@gmail.com>
2020-02-15 13:13:27 -0600

    not sure about this

--- front-end/budget-app/src/app/welcome/welcome.component.ts
+++ front-end/budget-app/src/app/welcome/welcome.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchdataService } from '../shared/services/fetchdata.service';
+
+
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'], 
+  // providers: [FetchdataService]
 })
 export class WelcomeComponent implements OnInit {
+  posts;
 
-  constructor() { }
+  Url = "/api/registerUser";
+
+  constructor(private dataService: FetchdataService) { }
+
+  getPosts() : void {
+    this.dataService.getData(this.Url)
+      .subscribe(data => this.posts = data)
+  }
 
   ngOnInit() {
+    this.getPosts();
   }
 
 }
